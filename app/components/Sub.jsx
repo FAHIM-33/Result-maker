29f122f755ef45dcdacfea62600bd6d5c3fa12cb
FAHIM-33 <shahriyerfahim2012@gmail.com>
2024-12-26 03:26:27 +0600

    update

--- app/components/Sub.jsx
+++ app/components/Sub.jsx
@@ -1,19 +1,29 @@
 'use client'
-import { useRef, useState } from "react";
 
-function Sub({ data, inputRef, idx, focusNext, focusPrev }) {
-    const [value, setValue] = useState(0)
+import { useState } from "react"
+
+function Sub({ subjectData, inputRef, idx, focusNext, focusPrev, setData }) {
+    const [grade, setGrade] = useState('')
 
     function handleChange(e) {
         const mark = e.target.value
+
+        subjectData.mark = mark
+
         if (/^\d{0,2}$/.test(mark)) {
-            setValue(mark);
-            document.getElementById(data.id).style.background = ''    
+            setGrade(getGrade(mark))
+            setData((sub) => {
+                const newArr = [...sub]
+                newArr[idx] = subjectData
+                return newArr
+            })
+
+            document.getElementById(subjectData.id).style.background = ''
             if (mark.length === 2) {
                 focusNext(idx);
             }
         } else {
-            document.getElementById(data.id).style.background = 'red'
+            document.getElementById(subjectData.id).style.background = 'red'
         }
     }
 
@@ -25,20 +35,51 @@ function Sub({ data, inputRef, idx, focusNext, focusPrev }) {
         }
     }
 
+    function getGrade(num) {
+        if (num >= 80) {
+            return "A+";
+        } else if (num >= 70) {
+            return "A";
+        } else if (num >= 60) {
+            return "A-";
+        } else if (num >= 50) {
+            return "B";
+        } else if (num >= 40) {
+            return "C";
+        } else if (num >= 33) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+
     return (
-        <div className=" w-fit">
-            <h2 className="font-bold text-center text-lg py-2">{data.title}</h2>
-            <input
-                id={data.id}
-                type="text"
-                className="w-full text-2xl p-1"
-                inputMode="numeric"
-                maxLength={2}
-                onChange={handleChange}
-                onKeyDown={handleKeydown}
-                ref={inputRef}
-            />
+        <div
+            className="w-[150px]  border rounded-md overflow-hidden">
+            <h2
+                onClick={() => { document.getElementById(subjectData.id).focus() }}
+                className="font-bold w-full mx-auto text-center text-lg py-2 text-nowrap">{subjectData.title}
+            </h2>
+            <div className="relative">{
+                grade &&
+                <p className={`absolute z-10 right-2 top-1/2 -translate-y-1/2 italic font-bold text-lg text-black ${grade === 'F' ? "text-red-500" : ''} text-center bg-[#fafafa67] rounded-full h-8 w-8`}>{grade}</p>
+            }
+                <input
+                    className="w-full text-xl p-1 mx-auto"
+                    id={subjectData.id}
+                    name={subjectData.name}
+                    type="text"
+                    inputMode="numeric"
+                    maxLength={2}
+                    onChange={handleChange}
+                    onKeyDown={handleKeydown}
+                    ref={inputRef}
+                />
+            </div>
+
         </div>
+
     );
 }
 
--- app/page.js
+++ app/page.js
@@ -1,20 +1,33 @@
 "use client"
 import Sub from "./components/Sub";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+const subjects = [
+  { id: 1, name: 'bangla', title: 'বাংলা', mark: '' },
+  { id: 2, name: 'english', title: 'English', mark: '' },
+  { id: 4, name: 'math', title: 'Math', mark: '' },
+  { id: 3, name: 'science', title: 'Science', mark: '' },
+  { id: 5, name: 'history', title: 'ইতিহাস', mark: '' },
+  { id: 6, name: 'religion', title: 'ধর্ম', mark: '' },
+  { id: 7, name: 'ict', title: 'ডিজিটাল প্রযুক্তি', mark: '' },
+  { id: 8, name: 'health', title: 'স্বাস্থ্য সুরক্ষা', mark: '' },
+  { id: 9, name: 'life', title: 'জীবন ও জীবিকা', mark: '' },
+  { id: 10, name: 'art', title: 'শিল্প ও সংস্কৃতি', mark: '' },
+]
 
 export default function Home() {
   const inputRefs = useRef([])
+  const [data, setData] = useState(subjects)
+  const [total, setTotal] = useState(0)
+  const [valid, setValid] = useState(false)
 
-  const subjects = [
-    { id: 1, name: 'bangla', title: 'Bangla', mark: '' },
-    { id: 2, name: 'english', title: 'English', mark: '' },
-    { id: 3, name: 'sci', title: 'Science', mark: '' },
-    { id: 4, name: 'math', title: 'গণিত', mark: '' },
-  ]
 
   function clearAll() {
     const inputs = document.getElementsByTagName('input')
     Object.values(inputs).map(elem => elem.value = '')
+    setData(subjects)
+    setTotal(0)
+    setValid(false)
+    document.getElementsByTagName('input')[0].focus()
   }
 
   const focusNext = (currentIdx) => {
@@ -28,27 +41,77 @@ export default function Home() {
     }
   }
 
+  
+  function cal() {
+    const a = data.reduce((acc, curr) => {
+      return acc + (parseInt(curr.mark) || 0);
+    }, 0);
+    setTotal(a);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  useEffect(() => {
+    const valiidity = data.every(sub => sub.mark !== '' && !isNaN(sub.mark * 1))
+    setValid(valiidity)
+  }, [data])
+
+  useEffect(() => {
+    const handleEnterKey = (e) => {
+      if (e.key === 'Enter') {
+        if (valid) {
+          cal()
+        } else { alert('Enter all values correctly') }
+      }
+    };
+    document.addEventListener('keydown', handleEnterKey)
+    return () => { document.removeEventListener('keydown', handleEnterKey) }
+  }, [cal, valid])
+
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <div className="border border-foreground flex t-container">
-        {
-          subjects.map((s, idx) =>
-            <Sub
-              key={s.id}
-              idx={idx}
-              data={s}
-              inputRef={(el) => (inputRefs.current[idx] = el)}
-              focusNext={focusNext}
-              focusPrev={focusPrev}
-            />
-          )
-        }
+    <div className="flex flex-col justify-start items-start lg:items-center lg:justify-center min-h-screen p-4">
+
+      <div className="flex flex-wrap justify-center gap-3 t-container lg:mt-0">
+        {data?.map((s, idx) => (
+          <Sub
+            key={s.id}
+            idx={idx}
+            subjectData={s}
+            inputRef={(el) => (inputRefs.current[idx] = el)}
+            focusNext={focusNext}
+            focusPrev={focusPrev}
+            setData={setData}
+          />
+        ))}
       </div>
-      <button
-        className="border border-red-500 rounded-md px-4 py-2 mt-12 hover:text-red-500 active:bg-red-500 active:text-white"
-        onClick={clearAll}
-      >Clear All</button>
+
+      {
+        total !== 0 ?
+          <div id="results" className="my-4 flex justify-evenly w-full ">
+            <p className="text-2xl">Total <span className="text-green-500">{total}</span></p>
+            <p className="text-2xl">GPA <span className="text-green-500">5.00</span></p>
+          </div>
+          :
+          <></>
+      }
+
+      <div className="flex gap-2 w-full mt-4">
+        <button
+          className="border border-red-500 rounded-md px-4 py-2 hover:text-red-500 active:bg-red-500 active:text-white w-full block"
+          onClick={clearAll}
+        >Clear All</button>
+        <button
+          onClick={cal}
+          id="cal"
+          className={`border rounded-md px-4 py-2 ${valid ? 'border-green-500 hover:text-green-400 active:bg-green-500' : 'border-gray-500 hover:text-gray-400 active:bg-gray-500 pointer-events-none'}  active:text-white w-full block ${valid ? 'opacity-100' : 'opacity-50'}`}
+        >
+          Calculate
+        </button>
+      </div>
     </div>
+
   );
 }
